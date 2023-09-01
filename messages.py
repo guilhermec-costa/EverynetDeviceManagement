53063c24a1c6691d6a63797363d1e747e22a4008
Guilherme China <echina725@gmail.com>
2023-09-01 00:56:36 -0300

    alterações na leitura de arquivos excel

--- messages.py
+++ messages.py
@@ -1,17 +1,22 @@
 
 class Colors:
     
-    def error(self, message):
-        print(f"\033[1;91m{message}\033[00m", end=' ')
+    def error(self, message, endline=False):
+        print(f"\033[1;91m{message}\033[00m") if endline is False \
+        else print(f"\033[1;91m{message}\033[00m", end=' ')
         
-    def warning(self, message):
-        print(f"\033[1;93m{message}\033[00m")
+    def warning(self, message, endline=False):
+        print(f"\033[1;93m{message}\033[00m") if endline is False \
+        else print(f"\033[1;93m{message}\033[00m", end=' ')
         
-    def prLightPurple(self, message):
-        print(f"\033[1;96m{message}\033[00m")
+    def prLightPurple(self, message, endline=False):
+        print(f"\033[1;96m{message}\033[00m") if endline is False \
+        else print(f"\033[1;96m{message}\033[00m", end=' ')
 
-    def prGreen(self, message):
-        print(f"\033[1;92m{message}\033[00m")
+    def prGreen(self, message, endline=False):
+        print(f"\033[1;92m{message}\033[00m") if endline is False \
+        else print(f"\033[1;92m{message}\033[00m", end=' ')
 
-    def prPurple(self, message):
-        print(f"\033[95m{message}\033[00m", end=' ')
+    def prPurple(self, message, endline=False):
+        print(f"\033[95m{message}\033[00m") if endline is False \
+        else print(f"\033[95m{message}\033[00m", end=' ')
